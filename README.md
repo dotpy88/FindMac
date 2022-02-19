c3fcc25cab0e4d1b49657c057e0f9e8a53d9ee9d
dotpy88 <tsmtbs07@gmail.com>
2022-02-19 05:21:28 -0600

    Added additional functionality

---
+++ README.md
@@ -0,0 +1,9 @@
+Getting started -
+
+1. pip install -r requirements.txt
+
+2. populate hostlist.csv with ip of switch in first column
+
+3. run getMacAddresses.py to generate bridge-tables.csv file
+
+
--- getMacAddresses.py
+++ getMacAddresses.py
@@ -2,32 +2,48 @@ from netmiko import ConnectHandler
 from dotenv import load_dotenv
 import re
 import os
+import sys
 
+### Load .env file to get password ###
 load_dotenv()
 
+### Cisco params used for netmiko package ###
 cisco_device = {
     'device_type': 'cisco_ios',
-    'host': '192.168.10.165',
+    'host': '',
     'username': 'tsmtbs07',
     'password': os.getenv('PASSWORD')
 }
 
-cisco_device_2 = {
-    'device_type': 'cisco_ios',
-    'host': '192.168.10.187',
-    'username': 'tsmtbs07',
-    'password': os.getenv('PASSWORD')
-}
+### Load hostlist.csv file to get ip address of switches ###
+all_devices = []
+csvfile = open('hostlist.csv','r')
+csvlines = csvfile.readlines()
+csvfile.close()
+for line in csvlines:
+    line = line.strip()
+    line = line.split(',')
+    all_devices.append(line[0])
 
-all_devices = [cisco_device,cisco_device_2]
-
+### Iterate through ip addresses and login to each switch.  Append to csvfile is found
+csvoutfile = 'bridge-tables.csv'
+csvfh = open(csvoutfile,'w')
+csvfh.write("Host,MacAddress,Port\n")
 for a_device in all_devices:
-    net_connect = ConnectHandler(**a_device)
+    cisco_device['host'] = a_device
+    net_connect = ConnectHandler(**cisco_device)
+
+    ### Send command to switch for bridge tables
     output = net_connect.send_command("show mac address-table")
-    print(f"\n\n--------- Device {a_device['host']} ---------")
+
+    ### Match mac addresses using regex
     mac_match = r"(\w{4}\.\w{4}\.\w{4})\s+\w+\s+(\S+)"
     mac_search = re.compile(mac_match,re.I)
     mac_addresses = mac_search.findall(output)
+
+    ### If macs are found, write them to csvfile
     if mac_addresses:
-        print(mac_addresses)
-    print("--------- End ---------") 
+        for mac in mac_addresses:
+            csvfh.write("%s,%s,%s\n" % (cisco_device['host'],mac[0],mac[1]))
+
+csvfh.close()
